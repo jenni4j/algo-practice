f5e4ebbb2a6d00462dace24a24fecc1332445e2e
jenni4j <jenni4j@gmail.com>
2023-08-10 12:21:49 -0700

    added sliding window technique

--- interview_prep/max_sum.py
+++ interview_prep/max_sum.py
@@ -1,16 +1,30 @@
 
 def max_sum_finder(arr: list[int], n: int, k: int) -> int:
-    max_sum = 0
-    for i in range(n - k + 1):
-        curr_sum = 0
-        for j in range(k):
-            curr_sum += arr[i + j]
-        max_sum = max(curr_sum, max_sum)
+	max_sum = 0
 
+	for i in range(n - k + 1):
+		curr_sum = 0
+		for j in range(k):
+			curr_sum += arr[i + j]
+		max_sum = max(curr_sum, max_sum)
+		
 
-    return max_sum
+def max_sum_window(arr: list[int], n: int, k: int) -> int:
+	if n < k:
+		return None
+	
+	window_sum = sum(arr[:k])
+	
+	max_sum = sum(arr[:k])
+	
+	for i in range(n - k):
+		window_sum = window_sum - arr[i] + arr[i + k]
+		max_sum = max(window_sum, max_sum)
 
+	return max_sum
+		
 
 if __name__ == '__main__':
-    res = max_sum_finder([1, 4, 2, 10, 2, 3, 1, 0, 20], 9, 4)
-    print(res)
+	res = max_sum_window([1, 4, 2, 10, 2, 3, 1, 0, 20], 9, 4)
+	res_again = max_sum_window([5, 2, -1, 0, 3], 5, 3)
+	print(res)
