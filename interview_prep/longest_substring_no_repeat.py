227c0fc75ed7443a62ad22056ff12b77779a2ef9
jenni4j <jenni4j@gmail.com>
2023-08-27 00:06:40 -0700

    final longest_substring_no_repeat

--- interview_prep/longest_substring_no_repeat.py
+++ interview_prep/longest_substring_no_repeat.py
@@ -18,31 +18,8 @@ def longest_substring_brute(s: str) -> int:
 
     return max_length
 
-def longest_substring(s: str) -> int:
-    max_length = 0 
-    left = 0
-    right = len(s) - 1
-    left_map = {}
-    right_map = {}
-
-    while left <= right:
-        if left_map.get(s[left]) is None and right_map.get(s[right]) is None:
-            if left == right:
-                left_map[s[left]] = left
-                left += 1
-            else:
-                left_map[s[left]] = left
-                right_map[s[right]] = right
-                left += 1
-                right -= 1
-        else:
-            break
-    
-
-    return max_length
-
-def longest_sub(s: str) -> int:
-    longest_substring = 0
+def longest_substring_init(s: str) -> int:
+    longest_substringstring = 0
     current_substring_length = 0
     some_map = {}
 
@@ -59,11 +36,11 @@ def longest_sub(s: str) -> int:
                 current_substring_length = length_to_previous_letter_occurence
             some_map[letter] = i
 
-        longest_substring = max(longest_substring, current_substring_length)
-    return longest_substring
+        longest_substringstring = max(longest_substringstring, current_substring_length)
+    return longest_substringstring
 
     
-def longest_sub(s: str) -> int:
+def longest_substring(s: str) -> int:
     letter_bank = {}
     left = 0
     right = 0
@@ -90,18 +67,18 @@ def longest_sub(s: str) -> int:
 
 
 if __name__ == '__main__':
-    assert longest_sub('abe') == 3, f"longest_sub('abe') == 3, found {longest_sub('abe')=}"
-    assert longest_sub('abcabcbb') == 3, f"longest_sub('abcabcbb') == 3, found {longest_sub('abcabcbb')=}"
-    assert longest_sub('bbbbb') == 1, f"longest_sub('bbbbb') == 1, found {longest_sub('bbbbb')=}"
-    assert longest_sub('abcc') == 3, f"expected longest_sub('abcc') == 3, instead {longest_sub('abcc')=}"
-    assert longest_sub('aabc') == 3, f"expected longest_sub('aabc') == 3, instead {longest_sub('aabc')=}"
-    assert longest_sub('abcck') == 3, f"expected longest_sub('abcck') == 3, instead {longest_sub('abcck')=}"
-    assert longest_sub('abczck') == 4, f"expected longest_sub('abczck') == 4, instead {longest_sub('abczck')=}"
+    assert longest_substring('abe') == 3, f"longest_substring('abe') == 3, found {longest_substring('abe')=}"
+    assert longest_substring('abcabcbb') == 3, f"longest_substring('abcabcbb') == 3, found {longest_substring('abcabcbb')=}"
+    assert longest_substring('bbbbb') == 1, f"longest_substring('bbbbb') == 1, found {longest_substring('bbbbb')=}"
+    assert longest_substring('abcc') == 3, f"expected longest_substring('abcc') == 3, instead {longest_substring('abcc')=}"
+    assert longest_substring('aabc') == 3, f"expected longest_substring('aabc') == 3, instead {longest_substring('aabc')=}"
+    assert longest_substring('abcck') == 3, f"expected longest_substring('abcck') == 3, instead {longest_substring('abcck')=}"
+    assert longest_substring('abczck') == 4, f"expected longest_substring('abczck') == 4, instead {longest_substring('abczck')=}"
 
-    assert longest_sub('pwwkew') == 3, f"expected longest_sub('pwwkew') == 3, instead {longest_sub('pwwkew')=}"
-    assert longest_sub('pwwwkp') ==  3, f"longest_sub('pwwwkp') == 3, found {longest_sub('pwwwkp')=}"
+    assert longest_substring('pwwkew') == 3, f"expected longest_substring('pwwkew') == 3, instead {longest_substring('pwwkew')=}"
+    assert longest_substring('pwwwkp') ==  3, f"longest_substring('pwwwkp') == 3, found {longest_substring('pwwwkp')=}"
 
-    assert longest_sub('contiguous') ==  7, f"longest_sub('contiguous') == 7, found {longest_sub('contiguous')=}"
+    assert longest_substring('contiguous') ==  7, f"longest_substring('contiguous') == 7, found {longest_substring('contiguous')=}"
     
-    assert longest_sub('haagen-dazs') ==  9, f"longest_sub('haagen-dazs') == 9, found {longest_sub('haagen-dazs')=}"
-    assert longest_sub('cnotiguous') ==  7, f"longest_sub('contiguous') == 7, found {longest_sub('contiguous')=}"
+    assert longest_substring('haagen-dazs') ==  9, f"longest_substring('haagen-dazs') == 9, found {longest_substring('haagen-dazs')=}"
+    assert longest_substring('cnotiguous') ==  7, f"longest_substring('contiguous') == 7, found {longest_substring('contiguous')=}"
