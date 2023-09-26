137c14eabc7abddd48d21073c7d5f2888544f0e7
jenni4j <jenni4j@gmail.com>
2023-09-26 11:31:59 -0700

    added longest substring repeat

--- interview_prep/longest_substring_repeat.py
+++ interview_prep/longest_substring_repeat.py
@@ -1,34 +1,19 @@
 def longest_substring(s: str, k: int) -> int:
-    left = 0
-    right = 1
-    longest_distance = 1
-    curr_distance = 1
-    curr_letter = s[left]
-    temp_k = k
-    temp_s = s
-    
-    while right < len(s):
-        if temp_s[right] == curr_letter:
-            curr_distance += 1
-            if curr_distance > longest_distance:
-                longest_distance = curr_distance 
-            right += 1
-        else:
-            if temp_k == 0:
-                temp_k = k
-                temp_s = s
-                left += 1
-                curr_letter = temp_s[left]
-                curr_distance -= 1
-            else:
-                temp_s[right] = curr_letter
-                temp_k -= 1
-                curr_distance += 1
-                if curr_distance > longest_distance:
-                    longest_distance = curr_distance
-                right += 1
-        
-    return longest_distance
+    l = 0
+    res = 0
+    letter_count = {}
+
+    for r in range(len(s)):
+        letter_count[s[r]] = 1 + letter_count.get(s[r], 0)
+
+        while (r - l + 1) - max(letter_count.values()) > k:
+            letter_count[s[l]] -= 1
+            l += 1
+
+        res = max(res, r - l + 1)
+
+    return res
+
 
 if __name__ == '__main__':
-    assert longest_substring('AAB', 1) == 3, f"longest substring = 3, found {longest_substring('AAB', 1)}"
+    assert longest_substring('ABAB', 2) == 4, f"longest substring = 4, found {longest_substring('ABAB', 2)}"
