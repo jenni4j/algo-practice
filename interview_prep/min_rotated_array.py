450e89c4b5502c92fa0c7911faa43081bdb37206
jenni4j <jenni4j@gmail.com>
2023-10-30 11:35:38 -0700

    review min search

--- interview_prep/min_rotated_array.py
+++ interview_prep/min_rotated_array.py
@@ -20,8 +20,31 @@ def min_rotated_search(nums: List[int]) -> int:
             left += 1
 
 
+def min_rotated_search_ii(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1
+    res = nums[0]
+
+    while left < right:
+        if nums[left] < nums[right]:
+            res = min(res, nums[left])
+            break
+        
+        mid = right // 2
+        res = min(res, nums[mid])
+
+        if nums[mid] > nums[left]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+
+
+    
+    return res
+
+
 if __name__ == '__main__':
-    res = min_rotated_search([1])
+    res = min_rotated_search([3, 4, 5, 1, 2])
     print(res)
 
     
